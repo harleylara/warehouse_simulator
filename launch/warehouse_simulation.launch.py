bb8292e24488c22594520cc4489c6ff6bbfdc3e4
SaimonMR <simon.martinez.rzs@gmail.com>
2025-11-17 12:29:31 -0300

    Added robot sensor and velocity command. Integrated with ROS2. Tested

--- launch/warehouse_simulation.launch.py
+++ launch/warehouse_simulation.launch.py
@@ -4,6 +4,7 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory, get_package_prefix
+from launch_ros.actions import Node 
 
 def generate_launch_description():
     pkg_share_path = get_package_share_directory('warehouse_simulator')
@@ -12,7 +13,7 @@ def generate_launch_description():
     lib_path = os.path.join(pkg_install_path, 'lib')
     world_path = os.path.join(pkg_share_path, 'worlds', 'tugbot_warehouse.sdf')
     yaml_path = os.path.join(pkg_share_path, 'config', 'actors_waypoints.yaml')
-    # os.environ['WAREHOUSE_SIMULATOR_YAML'] = yaml_path
+    rviz_config_path = os.path.join(pkg_share_path, 'viz', 'viz_warehouse.rviz')
 
     set_yaml = SetEnvironmentVariable(
         name='WAREHOUSE_SIMULATOR_YAML',
@@ -52,10 +53,37 @@ def generate_launch_description():
         launch_arguments={'gz_args': f'-r {world_path}'}.items()
     )
 
+    # ======= BRIDGE ROS2 <-> GAZEBO =======
+    # Careful using '/' in the model
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            # CMD_VEL y ODOM
+            '/robot/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+            '/robot/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry',
+            '/robot/camera/image@sensor_msgs/msg/Image@gz.msgs.Image', # CAMERA
+            '/robot/lidar2d/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan', # LIDAR 2D
+            '/robot/lidar3d/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked', # LIDAR 3D (PointCloud2)
+            '/robot/imu@sensor_msgs/msg/Imu@gz.msgs.IMU', # IMU
+        ],
+        output='screen'
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_path],
+        output='screen'
+    )
+
     return LaunchDescription([
-        set_yaml,           # <- primero, para que exista cuando arranca gz_sim
+        set_yaml,          
         set_plugin_path,
         set_ign,
         set_gz,
-        gz_launch
+        gz_launch,
+        bridge,
+        rviz_node
     ])
